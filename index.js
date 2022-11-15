b2ca234ff0274d43ce9eb0a63db89e7bb83ea42c
Shubh Johri <96282082+shubhvj96@users.noreply.github.com>
2022-11-15 12:07:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
  * @param {!express:Response} res HTTP response context.
  */
 exports.helloWorld = (req, res) => {
+  console.log("inside function");
   let message = req.query.message || req.body.message || 'Hello World! from github-gcp-actions';
   res.status(200).send(message);
 };
